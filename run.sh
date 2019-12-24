774852548941fbaa63c1e88cc20a88d00fcae4e1
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-12-24 13:57:58 -0500

    make text file + windows notification

--- run.sh
+++ run.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
-chmod +x install.sh
+#!/bin/bash -v
+chmod +x run.sh
 
 # Get directory name of `asemica.pl`
-PT= ln -s asemica.pl
+PT=$(ln -s asemica.pl)
+echo $PT >perlpath.txt
 
-# Install Python, Perl
 os=${OSTYPE//[0-9.-]*/}
 case "$os" in
 
-  darwin) # Apple/Mac
-  ;;
-
   msys) # Windows
+    python -v
+    perl -v
   ;;
 
   linux)
-  apt-get update
-  apt-get upgrade
-  apt-get install python3
-  apt-get install perl
+    apt-get update
+    apt-get upgrade
+    apt-get install python3
+    apt-get install perl
 
   exit 1
 esac
